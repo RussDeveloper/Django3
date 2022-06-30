f096e1c24bc084feeba158c6469fa289d60dc295
origir <https://github.com/RussDeveloper/django_project2.git>
2022-06-30 23:25:47 +0300

    Формы 2. Всё работает.

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', index, name='home'),
 
     path('news/<int:news_id>', view_news, name='view_news'),
+    path('news/add_news', add_news, name='add_news'),
 ]
 
 
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import News, Category
+from .forms import NewsForm
 
 # Create your views here.
 
@@ -22,3 +23,13 @@ def view_news(request, news_id):
     return render(request, 'news/view_news.html', {"news_item": news_item})
 
 
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)    #Забираем из формы данные
+        if form.is_valid():
+            #print(form.cleaned_data)
+            News.objects.create(**form.cleaned_data)  # Операция распаковки словаря в Python
+            return redirect('home')
+    else:
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form': form})
