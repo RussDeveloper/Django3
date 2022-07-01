2cc84eee5744c027d4e50832b31b96f62cee8ff1
origir <https://github.com/RussDeveloper/django_project2.git>
2022-07-01 14:59:36 +0300

    ListViev1

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from news.views import *
 
 urlpatterns = [
-    path('', index, name='home'),
-
+    #path('', index, name='home'),
+    path('', HomeNews.as_view(), name='home'),
     path('news/<int:news_id>', view_news, name='view_news'),
     path('news/add_news', add_news, name='add_news'),
 ]
